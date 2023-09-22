e44b1f793b299f0933c57838af3cb9c095e73090
YKyungmin <xcv556865@gmail.com>
2023-09-22 10:19:53 +0900

    d_2

--- main.js
+++ main.js
@@ -26,13 +26,25 @@ fetch('DB/department.json2')
 	});
 */
 
-fetch('DB/department.json2')
-	.then((data) => {
-		return data.json();
-	})
+const section = document.querySelector('section');
 
+fetch('DB/department.json')
+	.then((data) => data.json())
 	.then((json) => {
-		console.log(json);
+		let tags = '';
+		json.members.map((data) => {
+			tags += `
+      <article>
+      <div class="pic">
+        <img src="img/${data.pic}">
+      </div>
+      <h2>${data.name}</h2>
+      <p>${data.position}</p>
+    </article>
+      `;
+		});
+
+		section.innerHTML = tags;
 	})
 
 	.catch((err) => {
